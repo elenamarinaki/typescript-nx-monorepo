7ccbee8f02ac4d3e8046d77cb5e5869f8cfbcd7b
Elena Marinaki <76445935+elenamarinaki@users.noreply.github.com>
2022-01-08 17:02:39 +0000

    added pokemon.json with all the pokemons

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -8,7 +8,7 @@ import * as express from 'express';
 const app = express();
 
 app.get('/api', (req, res) => {
-  res.send({ message: 'Welcome to api!' });
+  res.send({ message: 'Welcome to api Elena!' });
 });
 
 const port = process.env.port || 3333;
